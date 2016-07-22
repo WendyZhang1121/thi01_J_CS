4d760ba0fa10d910cc9913caac08304f06e08346
Yuqi Zhang <yanwuzhidie@sina.com>
2016-07-22 23:12:05 +0100

    Creation

---
+++ src/thi01_J_CS/NetworkHandler.java
@@ -0,0 +1,29 @@
+package thi01_J_CS;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public final class NetworkHandler { 
+	private final ExecutorService executor;
+	
+	NetworkHandler(int poolSize) {
+		this.executor = Executors.newFixedThreadPool(poolSize);
+	}
+	
+	public void startThreads() { 
+		for (int i = 0; i < 3; i++) {
+			executor.execute(new HandleRequest()); 
+			}
+	}
+	
+	public void shutdownPool() { 
+		executor.shutdown();
+	}
+	
+	public static void main(String[] args) { 
+		NetworkHandler nh = new NetworkHandler(3); 
+		nh.startThreads();
+		nh.shutdownPool();
+	} 
+	
+}
